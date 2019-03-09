97ef8776109098d746d86df816be6bc25bcf89a8
SPFlores <sflores92886@gmail.com>
2019-03-09 12:20:32 -0800

    enemies into divs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,25 +21,42 @@ document.addEventListener('click', e => {
   let enemyPlayers = document.querySelector('#enemyPlayers')
   switch (e.target.id) {
     case 'fire':
-    chosenPlayer.innerHTML = `
-    <h3>${playables[0].name}</h3>
+      chosenPlayer.innerHTML = `
+    <h6>${playables[0].name}</h6>
       <br>
-        <img src=${playables[0].img} alt="${playables[0].text}"> 
+        <img class="options" src="${playables[0].picture}" alt="${playables[0].text}"> 
       <br>
       <p>${playables[0].healthpoints}</p>
     `
-    break
+      enemyPlayers.innerHTML = `
+    <h6>${playables[1].name}</h6>
+      <br>
+        <img class="options" src="${playables[1].picture}" alt="${playables[1].text}"> 
+      <br>
+      <p>${playables[1].healthpoints}</p>
+      <h6>${playables[2].name}</h6>
+      <br>
+        <img class="options" src="${playables[2].picture}" alt="${playables[2].text}"> 
+      <p>${playables[2].healthpoints}</p>
+      <h6>${playables[3].name}</h6>
+      <br>
+        <img class="options" src="${playables[3].picture}" alt="${playables[3].text}"> 
+      <br>
+      <p>${playables[3].healthpoints}</p>
+    `
+      console.log(e.target.id)
+      break
     case 'air':
       console.log(e.target.id)
-    break
+      break
     case 'water':
       console.log(e.target.id)
-    break
+      break
     case 'earth':
       console.log(e.target.id)
-    break
+      break
     default:
-    break
+      break
   }
 })
 
@@ -60,38 +77,38 @@ document.addEventListener('click', e => {
 // object of enemy stats/etc.
 let playables = [
   {
-  // < img class= "options" id = "fire" src = "./assets/images/fire.png" alt = "Fire" >
-  // <img class="options" id="air" src="./assets/images/air.png" alt="Air">
-  //   <img class="options" id="water" src="./assets/images/water.png" alt="Water">
-  //     <img class="options" id="earth" src="./assets/images/earth.png" alt="Earth">
-        name: 'Fire',
-        picture: "./assets/images/fire.png",
-        healthpoints: 150,
-        attackpower: 10,
-        counterattackpower: 20,
-      },
+    // < img class= "options" id = "fire" src = "./assets/images/fire.png" alt = "Fire" >
+    // <img class="options" id="air" src="./assets/images/air.png" alt="Air">
+    //   <img class="options" id="water" src="./assets/images/water.png" alt="Water">
+    //     <img class="options" id="earth" src="./assets/images/earth.png" alt="Earth">
+    name: 'Fire',
+    picture: "./assets/images/fire.png",
+    healthpoints: 150,
+    attackpower: 10,
+    counterattackpower: 20,
+  },
   {
-          name: 'Air',
-      picture: "./assets/images/air.png",
-      healthpoints: 150,
-      attackpower: 10,
-      counterattackpower: 20,
-    },
+    name: 'Air',
+    picture: "./assets/images/air.png",
+    healthpoints: 150,
+    attackpower: 10,
+    counterattackpower: 20,
+  },
   {
-          name: 'Water',
-      picture: "./assets/images/water.png",
-      healthpoints: 150,
-      attackpower: 10,
-      counterattackpower: 20,
-    },
+    name: 'Water',
+    picture: "./assets/images/water.png",
+    healthpoints: 150,
+    attackpower: 10,
+    counterattackpower: 20,
+  },
   {
-          name: 'Earth',
-      picture: "./assets/images/earth.png",
-      healthpoints: 150,
-      attackpower: 10,
-      counterattackpower: 20,
-    },
-  ]
-  
-  // run init
-  init()
+    name: 'Earth',
+    picture: "./assets/images/earth.png",
+    healthpoints: 150,
+    attackpower: 10,
+    counterattackpower: 20,
+  },
+]
+
+// run init
+init()
